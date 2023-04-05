475fd2c4b531d1b57b6a8cc655dbfe818a78ebb1
MauGutierrez <gerardo.gutierrezq@gmail.com>
2023-04-04 20:49:02 -0600

    added train of deep learning model in client

--- src/ros2_mnist/ros2_mnist/fl_client_1.py
+++ src/ros2_mnist/ros2_mnist/fl_client_1.py
@@ -1,5 +1,5 @@
-import rclpy # Python Client Library for ROS 2
-from rclpy.node import Node # Handles the creation of nodes
+import rclpy 
+from rclpy.node import Node 
 from ament_index_python.packages import get_package_share_directory
 from sensor_msgs.msg import Image
 from std_msgs.msg import Bool
@@ -8,14 +8,27 @@ from example_interfaces.srv import Trigger
 from cv_bridge import CvBridge
 import os
 import cv2
+import json
 import numpy as np
 import tensorflow as tf
 
 class FederatedClientA(Node):
-    def __init__(self):
+    def __init__(self, model_config):
         super().__init__('federated_client_a')
         
         self.model = None
+
+        # JSON file to store configuration parameters
+        self.model_config = model_config
+
+        # Dataset
+        self.dataset = None
+
+        self.X_test = None
+        self.X_train = None
+
+        self.y_test = None
+        self.y_train = None
 
         self.cli = self.create_client(Trigger, "get_model")
         while not self.cli.wait_for_service(timeout_sec=1.0):
@@ -27,21 +40,55 @@ class FederatedClientA(Node):
         rclpy.spin_until_future_complete(self, self.future)
         return self.future.result()
 
+    def preproces_data(self, dataset):
+        self.dataset = dataset
+        (self.X_train, self.y_train), (self.X_test, y_test) = self.dataset
+        self.X_train, self.X_test = self.X_train / 255.0, self.X_test / 255.0
+        
+
     def build_model(self):
         self.model = tf.keras.models.load_model("my_model.h5")
+        self.model.compile(
+            optimizer=self.model_config['optimizer'],
+            loss=self.model_config['loss'],
+            metrics=self.model_config['metrics']
+        )
         self.model.summary()
+    
+    def train_model(self):
+        self.model.fit(self.X_train, self.y_train, epochs=self.model_config['epochs'])
 
 def main():
-    rclpy.init()
+    # Load model hyperparameters
+    settings = os.path.join(get_package_share_directory('ros2_mnist'), "settings.json")
 
-    client = FederatedClientA()
-    response = client.send_request()
-    
-    if response.message == "Init download":
-        client.build_model()
+    # Load test dataset of mnist
+    mnist = tf.keras.datasets.mnist
+    dataset = mnist.load_data()
 
-    client.destroy_node()
-    rclpy.shutdown()
+    with open(settings) as fp:
+        content = json.load(fp)
+        hyperparameters = content['model_hyperparameters']
+
+        rclpy.init()
+
+        # Init client object to handle communication with server
+        client = FederatedClientA(hyperparameters)
+
+        # Preproces the dataset
+        client.preproces_data(dataset)
+
+        # Send an initial request to download the model from federated server 
+        response = client.send_request()
+        if response.message == "Init download":
+            # Build the deep learning model
+            client.build_model()
+
+            # Train the deep learning model
+            client.train_model()
+
+        client.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
